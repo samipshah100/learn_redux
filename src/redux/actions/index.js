f09b04d105df0790ce9d17df069524b9f6b52f74
Samip <samip227@gmail.com>
2021-09-14 17:53:53 +0530

    a

---
+++ src/redux/actions/index.js
@@ -0,0 +1,23 @@
+let nextTodoId = 0
+export const buyDosa = text => ({
+  type: 'BUY_DOSA',
+})
+export const buyIdli = text => ({
+  type: 'BUY_IDLI',
+})
+
+// export const setVisibilityFilter = filter => ({
+//   type: 'SET_VISIBILITY_FILTER',
+//   filter
+// })
+
+// export const toggleTodo = id => ({
+//   type: 'TOGGLE_TODO',
+//   id
+// })
+
+// export const VisibilityFilters = {
+//   SHOW_ALL: 'SHOW_ALL',
+//   SHOW_COMPLETED: 'SHOW_COMPLETED',
+//   SHOW_ACTIVE: 'SHOW_ACTIVE'
+// }
---
+++ src/redux/containers/Dhaba.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Dhaba() {
+    return (
+        <div>
+            <div>
+            Number of Dosas available : ${dosaCount}
+
+            </div>
+            <div>
+            Number of Idli available: ${idliCount}
+            </div>
+        </div>
+    )
+}
